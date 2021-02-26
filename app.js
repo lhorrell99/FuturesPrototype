41245fd8f4c839473ac30e946f5528154b6ed698
Louis Horrell <lhorrell99@gmail.com>
2021-02-26 18:03:10 +0000

    global heroku setup

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+let port = process.env.PORT; // required for heroku
+if (port == null || port == "") {
+    port = 8080;
+}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
